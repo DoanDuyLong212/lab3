f1242c8f63d31f069a37c6c7dcb3ea7d39cc4495
DoanDuyLong212 <155629275+DoanDuyLong212@users.noreply.github.com>
2024-01-27 20:29:56 +0700

    lab3q1.py

--- lab3q1.py
+++ lab3q1.py
@@ -1,8 +1,18 @@
 for i in range(1,6):
     print(*range(1,i+1))
-    
-tong = 0
-x = int(input('Enter number '))
-for i in range(x+1):
-    tong += i
-print('Sum is ',tong)
+
+
+def sum_of_numbers():
+    while True:
+        try:
+            n = int(input("Enter number: "))
+            if n < 1:
+                raise ValueError("Error: The input must be a positive integer.")
+            total = 0
+            for i in range(1, n + 1):
+                total += i
+            print("sum is", n, "is", total)
+            break
+        except ValueError as e:
+            print(e)
+sum_of_numbers()
